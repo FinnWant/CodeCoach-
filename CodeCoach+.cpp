cbf0d9e359a8e241fc50702d39da79dc06bfc894
Finnagin Wantland <fwantland50@gmail.com>
2026-03-14 12:25:54 -0700

    initial commit for CodeCoach+, includes JSON schema, project plan markdown, and the initial CodeCoach+.cpp

---
+++ CodeCoach+.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    //CodeCoach+ is a C++ program that helps code coaches keep track of good programs to use as student project examples over a variety of platforms such as
+    //Scratch, pixelpad, unity, and more. It allows code coaches to easily add new programs to the database, search for programs based on various criteria, and view details about each program.
+    //I want the program to be user friendly and easy to use, I plan on using uploading photos of code and using AI to analyze the code and extract relevant information
+    //Such as the programming language, the purpose of the program, and any notable features. I want to do this through APIs that can analyze code and extract relevant information. 
+    //I also want to have a QT based user interface that allows code coaches to easily add new programs to the database, search for programs based on various criteria, and view details about each program.
+    //I want to make JSON formats for the main 3 languages I want to support: Scratch, pixelpad, and unity. I want to use these JSON formats to store information about each program in the database that also viually looks like the program
+    // ex. Scratch _____________________      ex. Unity      Parent Object            ex. PixelPad      --- Class Start ---
+    //            | When GF is clicked |                     |--child Object 1                              *code*
+    //            | Move 10 steps      |                     |--child Object 2                          --- Class Loop ---
+    //            ---------------------                      |--child Object 3                              *code*             
+    //
+    //                                 
+} 
